a5177551355667103b27ff914878cd9277a034da
Elronnd <elronnd@elronnd.net>
2019-02-23 22:35:05 -0700

    Add logging

--- src/app.d
+++ src/app.d
@@ -1,6 +1,7 @@
 import std.stdio;
+import logging;
 
 void main() {
 	string commit_hash = import(".commit_hash.txt");
-	writefln("Currently running FancyEngine2 revision %s", commit_hash);
+	log("Currently running FancyEngine2 revision %s", commit_hash);
 }
---
+++ src/logging.d
@@ -0,0 +1,113 @@
+module logging;
+import std.stdio;
+
+import std.stdio: File, stderr;
+import std.string: format;
+
+
+enum LogLevel {
+	all, // pseudo-loglevel, just so you can set set_min_log_level(LogLevel.all)
+	trace, info, log, warning, error, critical, fatal
+}
+
+shared static this() {
+	set_logger_targets([stderr]);
+}
+
+private File[] log_targets;
+private LogLevel min_log_level = LogLevel.trace; // logs must be >this to be logged
+private LogLevel msg_log_level = LogLevel.error; // if logs are >this, show a message box
+
+void set_logger_targets(File[] files) {
+	synchronized log_targets = files;
+}
+void set_min_log_level(LogLevel ll) {
+	synchronized min_log_level = ll;
+}
+void set_msg_log_level(LogLevel ll) {
+	synchronized msg_log_level = ll;
+}
+void handle_log(LogLevel ll, int line, string file, string func_name, string pretty_func_name, string module_name, string msg) {
+	import std.datetime.systime: Clock;
+
+	string formatted_msg = format("\033[34m%s||%s||%s:%s:%s\033[31m|$\033[0m %s", Clock.currTime.toISOExtString(),
+			import(".commit_hash.txt")[0 .. $-1],
+			file,
+			func_name,
+			line,
+			msg);
+
+	if (ll >= min_log_level) {
+		foreach (target; log_targets) {
+			target.write(formatted_msg);
+		}
+	}
+	if (ll >= msg_log_level) {
+		import std.stdio;
+		writeln("TODO: show a message window");
+	}
+	if (ll == LogLevel.fatal) {
+		throw new Exception("Fatal message logged: " ~ formatted_msg);
+	}
+}
+
+
+// unabashadly stolen from phobos
+template log_funf(LogLevel ll) {
+	void log_funf(int line = __LINE__, string file = __FILE__,
+			string func_name = __FUNCTION__,
+			string pretty_func_name = __PRETTY_FUNCTION__,
+			string module_name = __MODULE__, A...)
+		(string msg, A args) {
+			handle_log(ll, line, file, func_name, pretty_func_name, module_name, format(msg, args));
+		}
+
+	void log_funf(int line = __LINE__, string file = __FILE__,
+			string func_name = __FUNCTION__,
+			string pretty_func_name = __PRETTY_FUNCTION__,
+			string module_name = __MODULE__, A...)
+		(bool condition, string msg, A args) {
+			if (condition) {
+				handle_log(ll, line, file, func_name, pretty_func_name, module_name, format(msg, args));
+			}
+		}
+}
+alias trace = log_funf!(LogLevel.trace);
+alias info = log_funf!(LogLevel.info);
+alias log = log_funf!(LogLevel.log);
+alias warning = log_funf!(LogLevel.warning);
+alias error = log_funf!(LogLevel.error);
+alias critical = log_funf!(LogLevel.critical);
+alias fatal = log_funf!(LogLevel.fatal);
+
+
+
+template log_funs(LogLevel ll) {
+	import std.conv: text;
+
+	void log_funs(int line = __LINE__, string file = __FILE__,
+			string func_name = __FUNCTION__,
+			string pretty_func_name = __PRETTY_FUNCTION__,
+			string module_name = __MODULE__, A...)
+		(A args) if ((args.length > 0 && !is(Unqual!(A[0]) : bool)) || args.length == 0) {
+			handle_log(ll, line, file, func_name, pretty_func_name, module_name, text(args));
+		}
+
+	void log_funs(int line = __LINE__, string file = __FILE__,
+			string func_name = __FUNCTION__,
+			string pretty_func_name = __PRETTY_FUNCTION__,
+			string module_name = __MODULE__, A...)
+		(bool condition, A args) {
+			if (condition) {
+				handle_log(ll, line, file, func_name, pretty_func_name, module_name, format(msg, args));
+			}
+		}
+}
+
+alias traces = log_funs!(LogLevel.trace);
+alias infos = log_funs!(LogLevel.info);
+alias logs = log_funs!(LogLevel.log);
+alias warnings = log_funs!(LogLevel.warning);
+alias errors = log_funs!(LogLevel.error);
+alias criticals = log_funs!(LogLevel.critical);
+alias fatals = log_funs!(LogLevel.fatal);
