bac9d45333bf5a251e801b8d08a2076cc83265d7
Elronnd <elronnd@elronnd.net>
2019-03-26 14:30:24 -0700

    Clarify fetch-libs.sh

--- fetch-libs.sh
+++ fetch-libs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 SDL2_VER="2.0.9"
-SDL2_FNAME="SDL2-$SDL2_VER-win32-x64.zip"
+SDL2_WIN_FNAME="SDL2-$SDL2_VER-win32-x64.zip"
 SDL2_URL_BASE="https://www.libsdl.org/release/"
 
 fetch-win() {
@@ -8,9 +8,9 @@ fetch-win() {
 	pushd lib/win
 
 	# SDL
-	wget "$SDL2_URL_BASE$SDL2_FNAME"
-	7z -aoa x $SDL2_FNAME
-	rm -f $SDL2_FNAME
+	wget "$SDL2_URL_BASE$SDL2_WIN_FNAME"
+	7z -aoa x $SDL2_WIN_FNAME
+	rm -f $SDL2_WIN_FNAME
 	mv README-SDL.txt .. #it asks me to distribute it.  I'm not mean!
 
 	popd
